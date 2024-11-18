372380a3020497b14838e140e239429543df071a
BraidenStock <stockbraiden@gmail.com>
2024-11-18 04:00:46 -0700

    Added basic UI elements such as HP/Ammo/Kill counter as well as damage effects
    the three squares should be replaced with the cracking effects. The scripts are not attached to things yet so they still need to be. remember to check for tags so the correct events trigger them.

---
+++ Assets/UI Elements/WinLose.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinLose : MonoBehaviour
+{
+    public Damage damage;
+    public Text GameOver;
+    public Text Victory;
+    // Static variable to store the count of destroyed enemies
+    public static int destroyedEnemiesCount = 0;
+
+    // If you want to display the count on the UI
+    public Text enemiesDestroyedText;
+
+    private void Start()
+    {
+        GameOver.gameObject.SetActive(false);
+        Victory.gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        // Update the UI text with the current count
+        if (enemiesDestroyedText != null)
+        {
+            enemiesDestroyedText.text = "Enemies killed: " + destroyedEnemiesCount + " / " + 40;
+        }
+
+        if(destroyedEnemiesCount >= 40)
+        {
+            Victory.gameObject.SetActive(true);
+        }
+
+        if (damage.IsDead())
+        {
+            GameOver.gameObject.SetActive(true);
+        }
+    }
+
+    // Method to be called when an enemy is destroyed
+    public static void EnemyDestroyed()
+    {
+        destroyedEnemiesCount++; // Increment the count of destroyed enemies
+    }
+}
