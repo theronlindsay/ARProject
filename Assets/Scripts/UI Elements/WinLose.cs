411a64797880e39acffdff0b01bbbfddd9077cce
theronlindsay <63890893+theronlindsay@users.noreply.github.com>
2024-11-26 11:30:30 -0700

    idk

--- Assets/Scripts/UI Elements/WinLose.cs
+++ Assets/Scripts/UI Elements/WinLose.cs
@@ -3,29 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using NUnit.Framework;
 
 public class WinLose : MonoBehaviour
 {
     public Damage damage;
-    public Text GameOver;
-    public Text Victory;
+    public TextMeshProUGUI GameOver;
+    public TextMeshProUGUI Victory;
     // Static variable to store the count of destroyed enemies
     public static int destroyedEnemiesCount = 0;
 
     // If you want to display the count on the UI
-    public Text enemiesDestroyedText;
+    public TextMeshProUGUI enemiesDestroyedText;
+
+    public GamePlayLoop gamePlayLoop;
 
     private void Start()
     {
         GameOver.gameObject.SetActive(false);
         Victory.gameObject.SetActive(false);
+        gamePlayLoop = GameObject.Find("EventSystem").GetComponent<GamePlayLoop>();
     }
     void Update()
     {
         // Update the UI text with the current count
         if (enemiesDestroyedText != null)
         {
-            enemiesDestroyedText.text = "Enemies killed: " + destroyedEnemiesCount + " / " + 40;
+            enemiesDestroyedText.text = "Enemies killed: " + destroyedEnemiesCount + " / " + 10 * gamePlayLoop.maxEnenmies;
         }
 
         if(destroyedEnemiesCount >= 40)
