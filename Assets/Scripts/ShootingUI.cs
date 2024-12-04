435b36753013e7790936186501784f7755584d64
theronlindsay <63890893+theronlindsay@users.noreply.github.com>
2024-12-03 17:03:07 -0700

    DECTECTING MESHES WORKS

--- Assets/Scripts/ShootingUI.cs
+++ Assets/Scripts/ShootingUI.cs
@@ -1,38 +1,24 @@
-using System.Collections;
-using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
+using System.Collections;
+using TMPro;
 public class ShootingUI : MonoBehaviour
 {
     //bullet
     public GameObject bullet;
 
-    
-
     //bullet force
     public float shootForce, upwardForce;
 
     //Gun Stuff
-    public float timeBetwenShooting, spread, timeBetweenShots;
+    public float timeBetwenShooting, spread, reloadTime, timeBetweenShots;
     public int bulletsPerTap;
     public bool allowButtonHold;
 
-    int bulletsShot;
-
-
-    public int maxAmmo = 10;           // Maximum ammo capacity
-    public int currentAmmo;            // Current ammo
-    public TextMeshProUGUI ammoDisplay;           // UI Text to display the ammo count
-    public float reloadTime = 2f;      // Time it takes to reload
-
-    private bool isreloading = false;  // Flag to check if reloading is in progress
-
-
-
+    int currentAmmo, bulletsShot;
 
     //bools
-    bool shooting, readyToShoot = true;
+    bool shooting, readyToShoot, isReloading;
 
     //Reference
     public Camera fpsCam;
@@ -40,96 +26,87 @@ public class ShootingUI : MonoBehaviour
 
     public InputActionReference touchscreen;
 
-    //public bool SF = false; //Allows the reload script to know when a bullet is fired
+    public int maxAmmo = 10;
+    public bool isreloading = false;
+    public TMPro.TextMeshProUGUI ammoDisplay;
 
     //bug fix
     public bool allowInvoke = true;
 
-    void Start()
-    {
-        currentAmmo = maxAmmo;
-        UpdateAmmoDisplay();
-    }
-
     private void Update()
     {
-        //MyInput();
-        //Shoot();
+        MyInput();
     }
 
-    //public void MyInput()
-    //{
-    //    //if touchscreen is pressed, shoot
-    //    if (touchscreen.action.triggered && readyToShoot && !isreloading && currentAmmo > 0)
-    //    {
-    //        Shoot();
-    //        Debug.Log("Restrictions Checked");
-    //    }
-    //}
+    private void MyInput()
+    {
+        //if touchscreen is pressed, shoot
+        if (touchscreen.action.triggered && readyToShoot && !isReloading && currentAmmo > 0)
+        {
+            Shoot();
+        }
+    }
 
     public void Shoot()
     {
-        if (readyToShoot && !isreloading && currentAmmo > 0)
+        Debug.Log("Shot");
+        readyToShoot = false;
+
+        //Finds the position to hit using raycast
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); 
+        RaycastHit hit;
+
+        //checks if ray hits something
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
         {
-            Debug.Log("Shot");
-            readyToShoot = false;
-
-            //Finds the position to hit using raycast
-            Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            RaycastHit hit;
-
-            //checks if ray hits something
-            Vector3 targetPoint;
-            if (Physics.Raycast(ray, out hit))
-            {
-                targetPoint = hit.point;
-            }
-            else
-            {
-                targetPoint = ray.GetPoint(75); //random point far away from player
-            }
-
-            //Calculate direction from attackPoint to targetPoint
-            Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
-
-            //Calculate spread
-            float x = Random.Range(-spread, spread);
-            float y = Random.Range(-spread, spread);
-
-            //Calculate direction with spread
-            Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
-
-            //Instantiate bullet/projectile
-            GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
-            //Rotate bullet to shoot direction
-            currentBullet.transform.forward = directionWithSpread.normalized;
-
-            //Rotate bullet 90 degrees in x axis
-            currentBullet.transform.Rotate(90, 0, 0);
-
-            //Add forces to bullet
-            currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
-            currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
-
-            currentAmmo--;
-            bulletsShot++;
-
-            //Invoke resetShot (if not done already)
-            if (allowInvoke)
-            {
-                Invoke("ResetShot", timeBetwenShooting);
-                allowInvoke = false;
-            }
-
-            //Update the UI
-            UpdateAmmoDisplay();
-
-            //if more than one bulletsPerTap make sure to repeat shoot function
-            ////if (bulletsShot < bulletsPerTap && currentAmmo > 0)
-            ////{
-            ////    Invoke("Shoot", timeBetweenShots);
-            ////}
+            targetPoint = hit.point;
         }
+        else
+        {
+            targetPoint = ray.GetPoint(75); //random point far away from player
+        }
+
+        //Calculate direction from attackPoint to targetPoint
+        Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
+
+        //Calculate spread
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        //Calculate direction with spread
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
+
+        //Instantiate bullet/projectile
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+        //Rotate bullet to shoot direction
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        //Rotate bullet 90 degrees in x axis
+        currentBullet.transform.Rotate(90, 0, 0);
+
+        //Add forces to bullet
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        currentAmmo--;
+        bulletsShot++;
+
+        //Invode resetShot (if not done already)
+        if(allowInvoke)
+        {
+            Invoke("ResetShot", timeBetwenShooting);
+            allowInvoke = false;
+        }
+
+        //if more than one bulletsPerTap make sure to repeat shoot function
+        if (bulletsShot < bulletsPerTap && currentAmmo > 0)
+        {
+            Invoke("Shoot", timeBetweenShots);
+        }
+
+        //Update UI
+        UpdateAmmoDisplay();
     }
 
     private void ResetShot()
@@ -157,7 +134,7 @@ public class ShootingUI : MonoBehaviour
         }
     }
 
-    IEnumerator reloadCoroutine()
+    private IEnumerator reloadCoroutine()
     {
         // Wait for the reload time (simulate reload animation/delay)
         yield return new WaitForSeconds(reloadTime);
--- Assets/Scripts/UI Elements/Reload.cs
+++ Assets/Scripts/UI Elements/Reload.cs
@@ -1,86 +1,73 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-//using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-//public class Reload : MonoBehaviour
-//{
-//    public int maxAmmo = 10;           // Maximum ammo capacity
-//    public int currentAmmo;            // Current ammo
-//    public Text ammoDisplay;           // UI Text to display the ammo count
-//    public float reloadTime = 2f;      // Time it takes to reload
+public class Reload : MonoBehaviour
+{
+    public int maxAmmo = 10;           // Maximum ammo capacity
+    public int currentAmmo;            // Current ammo
+    public Text ammoDisplay;           // UI Text to display the ammo count
+    public float reloadTime = 2f;      // Time it takes to reload
 
-//    private bool isreloading = false;  // Flag to check if reloading is in progress
-
-//    //public ShootingUI SUI;
+    private bool isreloading = false;  // Flag to check if reloading is in progress
     
-//    void Start()
-//    {
-//       // SUI = FindFirstObjectByType<ShootingUI>();
-//        // Initialize current ammo with max ammo at the start
-//        currentAmmo = maxAmmo;
-//        UpdateAmmoDisplay();
-//    }
+    void Start()
+    {
+        // Initialize current ammo with max ammo at the start
+        currentAmmo = maxAmmo;
+        UpdateAmmoDisplay();
+    }
 
-//    // Update is called once per frame
-//    public void Update()
-//    {
-//        if (Input.GetKeyDown(KeyCode.R) && !isreloading)
-//        {
-//            reload();
-//        }
-//    }
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !isreloading)
+        {
+            reload();
+        }
+    }
 
-//    public void reload()
-//    {
-//        if (currentAmmo < maxAmmo)
-//        {
-//            // Start reloading
-//            isreloading = true;
-//            Debug.Log("reloading...");
+    public void reload()
+    {
+        if (currentAmmo < maxAmmo)
+        {
+            // Start reloading
+            isreloading = true;
+            Debug.Log("reloading...");
 
-//            // Start reloading animation or delay
-//            StartCoroutine(reloadCoroutine());
-//        }
-//        else
-//        {
-//            Debug.Log("Ammo already full.");
-//        }
-//    }
+            // Start reloading animation or delay
+            StartCoroutine(reloadCoroutine());
+        }
+        else
+        {
+            Debug.Log("Ammo already full.");
+        }
+    }
 
-//    IEnumerator reloadCoroutine()
-//    {
-//        // Wait for the reload time (simulate reload animation/delay)
-//        yield return new WaitForSeconds(reloadTime);
+    IEnumerator reloadCoroutine()
+    {
+        // Wait for the reload time (simulate reload animation/delay)
+        yield return new WaitForSeconds(reloadTime);
 
-//        // After reloading, set current ammo to maxAmmo
-//        currentAmmo = maxAmmo;
-//        Debug.Log("reloaded! Ammo is full.");
+        // After reloading, set current ammo to maxAmmo
+        currentAmmo = maxAmmo;
+        Debug.Log("reloaded! Ammo is full.");
 
-//        // Update the UI
-//        UpdateAmmoDisplay();
+        // Update the UI
+        UpdateAmmoDisplay();
 
-//        // Set reloading flag to false
-//        isreloading = false;
-//    }
+        // Set reloading flag to false
+        isreloading = false;
+    }
 
-//    void UpdateAmmoDisplay()
-//    {
-//        // Update the ammo display text (you can modify this to use other UI components)
-//        if (ammoDisplay != null)
-//        {
-//            ammoDisplay.text = currentAmmo + " / " + maxAmmo;
-//        }
-//    }
-
-//    //public void ShotsFired()
-//    //{
-//    //    if SUI.Shoot())
-//    //    {
-//    //        currentAmmo -= currentAmmo;
-//    //        return 
-//    //    }
-
-//    //}
-//}
+    void UpdateAmmoDisplay()
+    {
+        // Update the ammo display text (you can modify this to use other UI components)
+        if (ammoDisplay != null)
+        {
+            ammoDisplay.text = currentAmmo + " / " + maxAmmo;
+        }
+    }
+}
