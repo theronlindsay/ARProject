c1bde5a4b0d19bec57759718051cc93dc06dd3bf
Nicholas Roy Richter <nicholasrichter2001@gmail.com>
2024-12-08 17:38:56 -0700

    CargoShipAI script
    Created the script for the Cargo Ship to circle around a random enemy spawner and drop Health and Sloth prefabs when hit. When both prefabs have been gotten, the ship despawns.
    Also edited the Bullet script to randomly call the DropHealth() and DropSloth() methods when the Cargo Ship is hit. Still need to implement how to determine which spawner the Cargo Ship will circle around.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,10 +7,18 @@ public class Bullet : MonoBehaviour
     private GamePlayLoop gamePlayLoop;
     private HeartHealth health;
 
+    bool gotHealth;
+    bool gotSloth;
+    int rand;
 
-    public void Start(){
+    public void Start()
+    {
         gamePlayLoop = GameObject.Find("EventSystem").GetComponent<GamePlayLoop>();
         health = GameObject.Find("HealthManager").GetComponent<HeartHealth>();
+
+        rand = Random.Range(0, 2);
+        gotHealth = false;
+        gotSloth = false;
     }
     public void OnCollisionEnter(Collision collision)
     {
@@ -22,6 +30,7 @@ public class Bullet : MonoBehaviour
 
             gamePlayLoop.EnemyKilled();
         }
+
         if (collision.gameObject.tag == "HealthPot")
         {
             health.Heal();
@@ -34,6 +43,36 @@ public class Bullet : MonoBehaviour
             SlowAllEnemies();
             Destroy(collision.gameObject);
             Destroy(gameObject);
+        }
+
+        if (collision.gameObject.GetComponent<CargoShipAI>())
+        {
+            if (rand == 0)
+            {
+                if (!gotHealth)
+                {
+                    collision.gameObject.GetComponent<CargoShipAI>().DropHealth();
+                    gotHealth = true;
+                }
+                else if (!gotSloth)
+                {
+                    collision.gameObject.GetComponent<CargoShipAI>().DropSloth();
+                    gotSloth = true;
+                }
+            }
+            else if (rand == 1)
+            {
+                if (!gotSloth)
+                {
+                    collision.gameObject.GetComponent<CargoShipAI>().DropSloth();
+                    gotSloth = true;
+                }
+                else if (!gotHealth)
+                {
+                    collision.gameObject.GetComponent<CargoShipAI>().DropHealth();
+                    gotHealth = true;
+                }
+            }
         }
     }
 
