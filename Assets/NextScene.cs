178ac308f8e0d4d639c03aba328b012612ad2cd1
theronlindsay <63890893+theronlindsay@users.noreply.github.com>
2024-12-10 01:04:23 -0800

    Builds now get past the video after 45 sec

---
+++ Assets/NextScene.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Video;
+using System.Collections;
+
+public class NextScene : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject skipButton;
+    public GameObject UI;
+
+    private void Start()
+    {
+        StartCoroutine(StartNextScene());
+        UI.SetActive(false);
+        skipButton.SetActive(true);
+    }
+
+    private IEnumerator StartNextScene()
+    {
+        yield return new WaitForSeconds(45);
+        skip();
+    }
+
+    public void skip()
+    {
+        videoPlayer.gameObject.SetActive(false);
+        skipButton.SetActive(false);
+        UI.SetActive(true);
+    }
+}
